a57689b97519db1b3597664cb64207ac10b33995
rong5026 <rong5026@naver.com>
2023-02-14 15:56:55 +0900

    [MOVE] orm파일 이동

---
+++ orm.js
@@ -0,0 +1,12 @@
+const SequelizeAuto = require('sequelize-auto');
+const auto = new SequelizeAuto("hackathondb", "root", "00000000", {
+      host: "hackathonserver.cynr3skzai4u.ap-northeast-2.rds.amazonaws.com",
+      port: "3306",
+      dialect: "mysql",
+      directory :".././models"
+      //noAlias: true // as 별칭 미설정 여부
+   }
+);
+auto.run((err)=>{
+   if(err) throw err;
+})
